87e4ad2a73f75f6cf7db319217d97973e8d684b1
jimmy <wjdtjq6@gmail.com>
2024-05-27 18:20:22 +0900

    Initial Commit

---
+++ TravelMagazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelMagazine
+//
+//  Created by t2023-m0032 on 5/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
